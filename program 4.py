8f297366584b1aa47ad4f502cfec03209f9c292e
borkarsampada40-stack <borkarsampada40@gmail.com>
2026-03-27 22:20:47 +0530

    Add files via upload

---
+++ program 4.py
@@ -0,0 +1,16 @@
+V = float(input("Enter voltage (V): "))
+R = float(input("Enter resistance (R): "))
+
+if R == 0:
+    print("Resistance cannot be zero.")
+else:
+    I = V / R
+
+    print(f"Current (I) = {I:.2f} A")
+
+    if I < 0.5:
+        print("Low current")
+    elif 0.5 <= I <= 2:
+        print("Normal current")
+    else:
+        print("High current")
